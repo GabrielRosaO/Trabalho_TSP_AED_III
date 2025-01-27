15f3116133b67d441b0bb7ef9f9c56c1cc88e698
G.Rosa <gbrielpank@gmail.com>
2025-01-27 14:51:19 -0300

    Adição de controle de tempo do algoritmo

--- src/App.java
+++ src/App.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
-
-import javax.annotation.processing.FilerException;
+import java.util.Timer;
+import java.util.Date;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -54,6 +53,7 @@ public class App {
             fileExact.close();
 
             if (algoOpcao == 1) {
+
                 // Resultado Exato
                 TSPExact exato = new TSPExact();
 
@@ -61,16 +61,15 @@ public class App {
 
                 exato.setCaminhoFinal();
 
-                long start = System.currentTimeMillis();
+                long start = System.nanoTime();
 
                 System.out.println("Custo total do Caminho: "
                         + exato.permutacaoDeCiclos(matrixTeste, caminhoInicial, verticeInicio, numVertices - 1));
 
-                long elapsed = System.currentTimeMillis() - start;
+                long elapsed = System.nanoTime() - start;
 
-                System.out.printf(
-                        "Tempo de execução em Segundos: %d\nTempo de execução em Milissegundos: %d\n\n",
-                        elapsed / 1000, elapsed);
+                System.out.printf("Tempo de execução em Segundos: %d s\nTempo de execução em Milissegundos: %d ms\n\n",
+                        elapsed / 1000000000, elapsed / 1000000);
 
             } else if (algoOpcao == 2) {
                 // Resultado Aproximado
@@ -78,17 +77,17 @@ public class App {
                 PrimTSP mst = new PrimTSP();
                 mst.setMatrixSize(numVertices);
 
-                long startAprox = System.currentTimeMillis();
+                long startAprox = System.nanoTime();
 
                 List<Integer> tspPath = mst.primTSP(matrixTeste);
 
-                long elapsedAprox = System.currentTimeMillis() - startAprox;
+                long elapsedAprox = System.nanoTime() - startAprox;
 
                 System.out.println("Custo total do caminho aproximado: " + mst.getCustoTotal());
                 // System.out.println("Caminho Aproximado do TSP: " + tspPath);
 
-                System.out.printf("Tempo de execução em Segundos: %d\nTempo de execução em Milissegundos: %d",
-                        elapsedAprox / 1000, elapsedAprox);
+                System.out.printf("Tempo de execução em Milissegundos: %d ms\nTempo de execução em Nanosegundos: %d ns",
+                        elapsedAprox / 1000000, elapsedAprox);
             }
         } catch (IOException error) {
             System.err.printf("Erro na abertura do arquivo: %s. \n", error.getMessage());
